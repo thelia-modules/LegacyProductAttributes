ef57ef88b03bc8809653100551f3af212f04a50d
Baptiste Cabarrou <bcabarrou@openstudio.fr>
2015-07-22 09:21:57 +0200

    Compatibility with Thelia 2.1 order product creation event

--- EventListeners/OrderAction.php
+++ EventListeners/OrderAction.php
@@ -4,11 +4,13 @@ namespace LegacyProductAttributes\EventListeners;
 
 use LegacyProductAttributes\Model\LegacyCartItemAttributeCombination;
 use LegacyProductAttributes\Model\LegacyCartItemAttributeCombinationQuery;
+use Propel\Runtime\ActiveQuery\Criteria;
 use Propel\Runtime\Exception\PropelException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Thelia\Core\Event\Order\OrderProductEvent;
+use Thelia\Core\Event\Order\OrderEvent;
 use Thelia\Core\Event\TheliaEvents;
 use Thelia\Model\OrderProductAttributeCombination;
+use Thelia\Model\OrderProductQuery;
 
 /**
  * Listener for order related events.
@@ -25,14 +27,37 @@ class OrderAction implements EventSubscriberInterface
     /**
      * Save the attribute combinations for the order from our cart item attribute combinations.
      *
-     * @param OrderProductEvent $event
+     * @param OrderEvent $event
      *
      * @throws PropelException
      */
-    public function createOrderProductAttributeCombinations(OrderProductEvent $event)
+    public function createOrderProductAttributeCombinations(OrderEvent $event)
     {
         $legacyCartItemAttributeCombinations = LegacyCartItemAttributeCombinationQuery::create()
             ->findByCartItemId($event->getCartItemId());
+
+        // works with Thelia 2.2
+        if (method_exists($event, 'getId')) {
+            $orderProductId = $event->getId();
+        } else {
+            // Thelia 2.1 however does not provides the order product id in the event
+
+            // Since the order contains potentially identical (for Thelia) cart items that are only differentiated
+            // by the cart item attribute combinations that we are storing ourselves, we cannot use information
+            // such as PSE id to cross reference the cart item we are given to the order product that was created from
+            // it (as far as I can tell).
+
+            // So we will ASSUME that the last created order product is the one created from this cart item.
+            // This is PROBABLY TRUE on a basic Thelia install with no modules messing with the cart and orders in a way
+            // that create additional order products, BUT NOT IN GENERAL !
+
+            // FIXME: THIS IS NOT A SANE WAY TO DO THIS
+
+            $orderProductId = OrderProductQuery::create()
+                ->orderByCreatedAt(Criteria::DESC)
+                ->findOne()
+                ->getId();
+        }
 
         /** @var LegacyCartItemAttributeCombination $legacyCartItemAttributeCombination */
         foreach ($legacyCartItemAttributeCombinations as $legacyCartItemAttributeCombination) {
@@ -40,7 +65,7 @@ class OrderAction implements EventSubscriberInterface
             $attributeAv = $legacyCartItemAttributeCombination->getAttributeAv();
 
             (new OrderProductAttributeCombination())
-                ->setOrderProductId($event->getId())
+                ->setOrderProductId($orderProductId)
                 ->setAttributeTitle($attribute->getTitle())
                 ->setAttributeChapo($attribute->getChapo())
                 ->setAttributeDescription($attribute->getDescription())
