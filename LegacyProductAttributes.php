21385119602d14ab2bb8d0d68d542e4e04828ac0
Baptiste Cabarrou <bcabarrou@openstudio.fr>
2015-08-13 14:34:42 +0200

    Safer tables creation and deletion

--- LegacyProductAttributes.php
+++ LegacyProductAttributes.php
@@ -12,10 +12,21 @@ class LegacyProductAttributes extends BaseModule
     const MESSAGE_DOMAIN_BO = 'legacyproductattributes.bo.default';
     const MESSAGE_DOMAIN_FO = 'legacyproductattributes.fo.default';
 
-    public function postActivation(ConnectionInterface $con = null)
+    public function preActivation(ConnectionInterface $con = null)
     {
         $database = new Database($con);
 
-        $database->insertSql(null, [__DIR__ . "/Config/thelia.sql"]);
+        $database->insertSql(null, [__DIR__ . "/Config/create.sql"]);
+    }
+
+    public function destroy(ConnectionInterface $con = null, $deleteModuleData = false)
+    {
+        if (!$deleteModuleData) {
+            return;
+        }
+
+        $database = new Database($con);
+
+        $database->insertSql(null, [__DIR__ . "/Config/delete.sql"]);
     }
 }
