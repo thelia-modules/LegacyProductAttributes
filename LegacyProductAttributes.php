9adf8c3bbf4f69e8e21946a9ac31d2ea1c17a821
Baptiste Cabarrou <bcabarrou@openstudio.fr>
2015-10-08 14:02:24 +0200

    Add missing return value from preActivation

--- LegacyProductAttributes.php
+++ LegacyProductAttributes.php
@@ -17,6 +17,8 @@ class LegacyProductAttributes extends BaseModule
         $database = new Database($con);
 
         $database->insertSql(null, [__DIR__ . "/Config/create.sql"]);
+
+        return true;
     }
 
     public function destroy(ConnectionInterface $con = null, $deleteModuleData = false)
