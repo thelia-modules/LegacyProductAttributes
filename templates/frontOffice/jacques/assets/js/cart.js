e7777c484132c478a3507ad5324ca2ac703572d1
Franck Allimant <github@allimant.org>
2017-07-03 18:10:48 +0200

    Deleted useless templates

--- templates/frontOffice/jacques/assets/js/cart.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    var $inserts = $('.legacy-product-attributes-cart-item-attribute-combination');
-    var i = 0;
-
-    $('.table-cart').find('tbody').find('tr').each(function() {
-        $(this).find('.product-options').find('dl').append($($inserts.get(i)).html());
-        ++i;
-    });
-})(jQuery);
